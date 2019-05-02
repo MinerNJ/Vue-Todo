9e64773c2150ed33250c0f4089f6e32fa931efea
Nick Miner <Nick.Miner18@gmail.com>
2019-05-02 17:42:22 -0400

    todo app

---
+++ src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <todo v-for="(todo, key) in todos" v-bind:todo="todo" v-bind:key="key"></todo>
+    <todo v-on:delete-todo="deleteTodo" v-for="(todo, key) in todos" v-bind:todo="todo" v-bind:key="key"></todo>
+    <todo v-on:complete-todo="completeTodo" v-for="(todo, key) in todos" v-bind:todo="todo" v-bind:key="key"></todo>
+
+
+  </div>
+</template>
+
+<script type = "text/javascript" >
+
+import Todo from './Todo';
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo,
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+
+    completeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = true;
+    },
+  },
+};
+</script>
